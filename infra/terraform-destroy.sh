7ef2b0acbbdb7ba5b5d094df91527aff58a823e6
skyglass <skyglass2001@gmail.com>
2025-06-04 02:15:17 +0200

    - final decision on which Dockerfile use for production "moviebets-ui" (will use the same Dockerfile as for local environment to avoid rewriting the whole environment variables logic, like I did with the React (enough experiments with frontend development, this is not my strongest asset)

--- infra/terraform-destroy.sh
+++ infra/terraform-destroy.sh
@@ -1,9 +1,3 @@
-kubectl delete ingress ingress-service
-kubectl delete service moviebets-postgres
-kubectl delete service kafka-ui
 helm uninstall cert-manager --namespace public-ingress
 helm uninstall ingress-nginx --namespace public-ingress
-kubectl delete deployment keycloak-postgres
-kubectl delete kafkanodepool kafka
-kubectl delete kafka my-cluster
 terraform destroy --auto-approve
